39e96bacd964fd5626f8b7e25b9a5cc20ad18c39
simamu530 <58772879+simamu530@users.noreply.github.com>
2022-01-23 19:52:07 +0900

    Create 2022_01_23_103313_create_savesetting_table.php

---
+++ database/migrations/2022_01_23_103313_create_savesetting_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSavesettingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('savesetting', function (Blueprint $table) {
+            $table->id();
+            $table->string('game_title');
+            $table->string('mouse')->default(0);
+            $table->integer('dpi');
+            $table->double('mousesens');
+            $table->double('multisens');
+            $table->integer('hz')->default(0);
+            $table->string('mousepad')->default(0);
+            $table->string('keyboard')->default(0);
+            $table->integer('fov')->default(0);
+            $table->timestamp('created_at')->useCurrent()->nullable();
+            $table->timestamp('updated_at')->useCurrent()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('savesetting');
+    }
+}
