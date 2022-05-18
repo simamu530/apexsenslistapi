f5216b39975e78b49cf84109f28d12354acaba89
simamu530 <58772879+simamu530@users.noreply.github.com>
2022-05-19 07:23:52 +0900

    admin success

--- app/Http/Controllers/AdminUserController.php
+++ app/Http/Controllers/AdminUserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
@@ -9,12 +10,14 @@ class AdminUserController extends Controller
 {
     public function postRegister(Request $data)
     {
-        AdminUserController::create([
+        User::create([
             'admin' => true,
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
-        return redirect('/home');
+        return response()->json([
+            'data' => 'success'
+        ], 200);
     }
 }
