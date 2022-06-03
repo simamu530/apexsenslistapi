b7beeef8552d7e4520f9be28f89a240ee54ee334
simamu530 <58772879+simamu530@users.noreply.github.com>
2022-06-03 12:42:33 +0900

    message

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,8 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->boolean('admin')->default(false);//管理者フラグ追加
-            $table->id();
+            $table->boolean('admin')->default(false);
+            $table->string('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
