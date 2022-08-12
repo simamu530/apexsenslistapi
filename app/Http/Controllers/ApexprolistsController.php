dcbb83576d86fab2b06119e9fb871dbd70ef943f
simamu530 <58772879+simamu530@users.noreply.github.com>
2022-08-12 19:44:43 +0900

    message

--- app/Http/Controllers/ApexprolistsController.php
+++ app/Http/Controllers/ApexprolistsController.php
@@ -28,23 +28,23 @@ class ApexprolistsController extends Controller
      */
     public function store(Request $request)
     {
-        // $validate_rule = [
-        //     'team' => 'required',
-        //     'name' => 'required',
-        //     'dpi' => 'required',
-        //     'mousesens' => 'required',
-        //     'multisens' => 'required',
-        //     'hz' => 'required',
-        //     'fov' => 'required',
-        //     'mouse' => 'required',
-        //     'monitor' => 'required',
-        //     'gpu' => 'required',
-        //     'resolution' => 'required',
-        //     'mousepad' => 'required',
-        //     'keyboard' => 'required',
-        //     'headset' => 'required',
-        // ];
-        // $this->validate($request, $validate_rule);
+        $validate_rule = [
+            'team' => 'required',
+            'name' => 'required',
+            'dpi' => 'required',
+            'mousesens' => 'required',
+            'multisens' => 'required',
+            'hz' => 'required',
+            'fov' => 'required',
+            'mouse' => 'required',
+            'monitor' => 'required',
+            'gpu' => 'required',
+            'resolution' => 'required',
+            'mousepad' => 'required',
+            'keyboard' => 'required',
+            'headset' => 'required',
+        ];
+        $this->validate($request, $validate_rule);
         $item = Apexprolist::create($request->all());//新規作成
         return response()->json([
             'data' => $item
